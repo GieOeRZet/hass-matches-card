26dc2cad85bde4f44b650abfecfd756c82554156
GieOeRZet <57875682+GieOeRZet@users.noreply.github.com>
2025-11-03 08:48:15 +0100

    Add constants for card and league logos

---
+++ src/const.ts
@@ -0,0 +1,11 @@
+export const CARD_VERSION = "0.3.010";
+export const CARD_NAME = "matches-card";
+export const CARD_EDITOR_NAME = "matches-card-editor";
+
+export const LEAGUE_LOGO_BASE =
+  "https://raw.githubusercontent.com/GieOeRZet/matches-card/main/logo";
+
+export const LEAGUE_LOGOS: Record<string, string> = {
+  L: `${LEAGUE_LOGO_BASE}/ekstraklasa.png`,
+  PP: `${LEAGUE_LOGO_BASE}/puchar.png`
+};

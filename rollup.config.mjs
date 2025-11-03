348b7663bccc7e635f778942ee6d11f0fae94809
GieOeRZet <57875682+GieOeRZet@users.noreply.github.com>
2025-11-03 09:40:11 +0100

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,8 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
+import { terser } from "@rollup/plugin-terser";
+
 
 export default {
   input: "src/matches-card.ts",
